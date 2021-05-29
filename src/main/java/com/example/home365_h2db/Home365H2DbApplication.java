0682ef9db1aa7eda06a859206ca7992d609597ac
Shaulib <bleich.shauli@gmail.com>
2021-05-29 22:04:36 +0300

    adding swagger-ui

--- src/main/java/com/example/home365_h2db/Home365H2DbApplication.java
+++ src/main/java/com/example/home365_h2db/Home365H2DbApplication.java
@@ -8,28 +8,17 @@ import org.springframework.boot.ConfigurableBootstrapContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.util.LinkedList;
 import java.util.List;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Home365H2DbApplication {
 
     public static void main(String[] args) {
-//        ConfigurableApplicationContext configurableApplicationContext =
         SpringApplication.run(Home365H2DbApplication.class, args);
-//        DestinationRepository destinationRepository = configurableApplicationContext.getBean(DestinationRepository.class);
-//        AircraftRepository aircraftRepository = configurableApplicationContext.getBean(AircraftRepository.class);
-//        Aircraft myAircraft = new Aircraft("El-Al",4,50000,500);
-//        Aircraft my2ndAircraft = new Aircraft("Turkish-Airlines",14,30000, 380);
-//        List<Aircraft> myList = new LinkedList<Aircraft>();
-//        myList.add(myAircraft);
-//        myList.add(my2ndAircraft);
-//        aircraftRepository.saveAll(myList);
-//        Destination arizona = new Destination("Arizona", 34.048927, -111.093735);
-//        Destination jerusalem = new Destination("Jerusalem", 31.692651, 35.284082);
-//        destinationRepository.save(arizona);
-//        destinationRepository.save(jerusalem);
-    }
+      }
 
 }
